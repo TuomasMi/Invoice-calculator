ab435066c84166c37a5fa5422272c1ce08c75d7a
Tuomas <tuomas.o.mikkola@tuni.fi>
2021-06-16 02:08:41 +0300

    Add reading from .csv and checking almost cases

--- Invoice-calculator/main.cpp
+++ Invoice-calculator/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <string>
+#include <locale>
+#include <math.h>
 
 
 
-void subsetsUtil(std::vector<float>& A, std::vector<std::vector<std::pair<float, int>> >& res,
-                 std::vector<std::pair<float,int>>& subset, int index)
+void subsetsUtil(std::vector<int>& A, std::vector<std::vector<std::pair<int, int>> >& res,
+                 std::vector<std::pair<int,int>>& subset, int index)
 {
     res.push_back(subset);
     for (int i = index; i < A.size(); i++) {
@@ -25,10 +29,10 @@ void subsetsUtil(std::vector<float>& A, std::vector<std::vector<std::pair<float,
 }
 
 // below function returns the subsets of vector A.
-std::vector<std::vector<std::pair<float, int>> > subsets(std::vector<float>& A)
+std::vector<std::vector<std::pair<int, int>> > subsets(std::vector<int>& A)
 {
-    std::vector<std::pair<float, int>> subset;
-    std::vector<std::vector<std::pair<float, int>> > res;
+    std::vector<std::pair<int, int>> subset;
+    std::vector<std::vector<std::pair<int, int>> > res;
 
     // keeps track of current element in vector A;
     int index = 0;
@@ -37,51 +41,116 @@ std::vector<std::vector<std::pair<float, int>> > subsets(std::vector<float>& A)
     return res;
 }
 
-bool checkSum(std::vector<std::pair<float, int>>& inputs, float& target)
+bool checkSum(std::vector<std::pair<int, int>>& inputs, int& target)
 {
-    float sum = 0;
+    int sum = 0;
     for (auto input : inputs) {
         sum += input.first;
     }
     return sum == target;
 }
 
-void run()
+int closeEnough(std::vector<std::pair<int, int>>& inputs, int& target)
 {
-    std::vector<float> invoices;
-    float input;
-    float target;
-
-
-    // Read invoices to vector
-    std::cout << "Give invoices, end with f: ";
-    while (std::cin >> input) {
-        invoices.push_back(input);
+    int sum = 0;
+    for (auto input : inputs) {
+        sum += input.first;
     }
 
-    std::cin.clear();
-    std::cin.ignore(1000,'\n');
+    if (sum <= target + 100 and sum > target) {
+        return sum - target;
+    }
+
+    else if (sum >= target - 100 and sum < target) {
+        return sum - target;
+    }
+
+    else {
+        return 0;
+    }
+
+}
+
+void run()
+{
+    std::vector<int> invoices;
+    float target;
+
+    std::ifstream inputFile;
+    inputFile.open("input.csv");
+
+
+    // Read invoices manually to vector
+    /*float input;
+     std::cout << "Give invoices, end with f: ";
+    while (std::cin >> input) {
+        invoices.push_back(input);
+    }*/
+
+    // read .csv file to vector
+    while (inputFile.good()) {
+        std::string line;
+        std::getline(inputFile, line);
+        if (line != "") {
+            for (char& c : line) {
+                if (c == ',') {
+                    c = '.';
+                }
+            }
+            float value = std::stof(line);
+            value = ceil(value * 100);
+            int ivalue = static_cast<int>(value);
+            invoices.push_back(ivalue);
+        }
+
+    }
+
+    inputFile.close();
+
+    //std::cin.clear();
+    //std::cin.ignore(1000,'\n');
     std::cout << "Give target amount: ";
     std::cin >> target;
+    target = ceil(target * 100);
+    int itarget = static_cast<int>(target);
 
-    std::vector<std::vector<std::pair<float, int>>> res = subsets(invoices);
+    std::vector<std::vector<std::pair<int, int>>> res = subsets(invoices);
 
     bool numbersFound = false;
 
     for (auto subset : res) {
-        if (checkSum(subset, target)) {
+        if (checkSum(subset, itarget)) {
             std::cout << "\n";
             for (auto result : subset) {
-                std::cout << result.second + 1 << " " << result.first <<"\n";
+                std::cout << result.second + 1 << " " << static_cast<float>(result.first) / 100 <<"\n";
             }
             numbersFound = true;
         }
 
     }
 
-    if (not numbersFound) {
+    bool closeFound = false;
+
+    if (!numbersFound) {
+
+        for (auto subset : res) {
+            int result = closeEnough(subset, itarget);
+            if (result != 0) {
+                std::cout << "\nNot exact, but almost: \n";
+                for (auto r : subset) {
+                    std::cout << r.second + 1 << " " << static_cast<float>(r.first) / 100 << "\n";
+                }
+                std::cout << "Separated by: " << static_cast<float>(result) / 100 << "\n";
+                closeFound = true;
+            }
+        }
+
+    }
+
+    if (!numbersFound && !closeFound){
         std::cout << "No possible solutions \n";
     }
+
 }
 
 int main()
